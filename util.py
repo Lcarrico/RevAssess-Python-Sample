2779404681ae9f89e87f936180e0da9246643969
Leo Carrico <lcarrico827@gmail.com>
2021-04-20 11:51:19 -0500

    fixed zip file

--- util.py
+++ util.py
@@ -2,7 +2,9 @@ import pytest
 import json
 import base64
 import requests
-from os import path
+from os import path, remove, listdir, rmdir, mkdir
+import shutil
+
 
 def RevaTest(points=0, tier=0):
     def decorator_reva_test(func):
@@ -42,13 +44,16 @@ def load_result():
         tests = json.load(f)
     with open("RevAssessConfig.json") as f:
         config = json.load(f)
-    with open ("main.py", "r") as f:
-        main=f.read()
+
+    zip()
+
+    with open("main.zip", "rb") as f:
+        main_bytes = f.read()
     
-    main_bytes = main.encode('ascii')
+    # remove("main.zip")
+    
     base64_bytes = base64.b64encode(main_bytes)
     base64_main = base64_bytes.decode('ascii')
-
 
     data['config'] = config
     data['tests'] = tests
@@ -91,3 +96,35 @@ def add_to_json(testName, points, isSuccessful, errorMessage, tier):
 
     with open(filename, 'w') as outfile:
         json.dump(data, outfile)
+
+def zip():
+    if path.exists("main.zip"):
+        remove("main.zip")
+    
+    dest = copytree()
+
+    shutil.make_archive("main", 'zip', dest)
+
+    shutil.rmtree(dest)
+
+
+def copytree():
+
+    src = "."
+    dst = "to_zip"
+
+    mkdir(dst)
+
+    exclude = ['__pycache__', '.pytest_cache', '.git', 'to_zip']
+
+    for item in listdir(src):
+        if item in exclude:
+            continue
+        s = path.join(src, item)
+        d = path.join(dst, item)
+        if path.isdir(s):
+            shutil.copytree(s, d, True)
+        else:
+            shutil.copy2(s, d) 
+    
+    return dst
