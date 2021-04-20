65ac54a2b1f50bddfdb4529b66975f58284f9c42
Leo Carrico <lcarrico827@gmail.com>
2021-04-20 09:34:40 -0500

    init repo

---
+++ test.py
@@ -0,0 +1,16 @@
+import pytest
+from main import *
+from util import RevaTest
+
+
+@RevaTest(points=10, tier=0)
+def test_add():
+    assert add(3,4) == 7
+    assert add(-10, 20) == 10
+    
+@RevaTest(points=10, tier=0)
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(2, 5) == -3
+
+
